b9e8777bbce58de30effa6f494957f61d65d01af
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-20 12:06:33 -0500

    Completed Exercise 6
    Completed Exercise 6

---
+++ Class 8-Exercise 6/main.js
@@ -0,0 +1,12 @@
+// Loop through the provided list of items.
+// For each item, create a new li element, set its text to the item's name, and append it to the provided ul.
+// Ensure that the background color of every even item is "gray". Starting Code:
+// <ul id="itemList"></ul>
+// <script>
+//     const items = [
+//         {name: 'Item 1'},
+//         {name: 'Item 2'},
+//         {name: 'Item 3'},
+//         {name: 'Item 4'}
+//     ];
+// </script>
