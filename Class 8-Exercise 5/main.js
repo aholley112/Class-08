15050ed7b42351ccb06274969db6db158abff042
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-20 12:02:22 -0500

    Completed Exercise 5
    Completed Exercise 5

---
+++ Class 8-Exercise 5/main.js
@@ -0,0 +1,9 @@
+// For the provided input element, add an event listener to log every keypress made by the user.
+// Display the current content of the input in a separate paragraph below the input every time the user types. Starting Code:
+
+
+// Starting Code:
+
+{/* <input type="text" id="textInput" />
+
+<p id="textDisplay"></p> */}
